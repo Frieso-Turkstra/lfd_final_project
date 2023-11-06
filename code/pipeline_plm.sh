a5664bc6485e2b021eb026ddccc17d4c9d4874dd
Marieke van der Holt <mariekevanderholt@gmail.com>
2023-11-06 17:56:23 +0100

    change model to twitter

--- code/pipeline_plm.sh
+++ code/pipeline_plm.sh
@@ -1,2 +1,2 @@
-python test_PLM.py -in ../data/twitter/test.tsv -telegram
+python test_PLM.py -in ../data/twitter/test.tsv -twitter
 python evaluate.py -i predictions.json -v ../data/twitter/test.tsv
