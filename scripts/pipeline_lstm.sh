a6ecdace2f0e98d942ee15a2daa59c8645e7d5a0
Frieso Turkstra <friesoturkstra@gmail.com>
2023-11-06 18:26:12 +0100

    Update pipeline_lstm.sh

--- scripts/pipeline_lstm.sh
+++ scripts/pipeline_lstm.sh
@@ -1,2 +1,2 @@
-python train_LSTM.py -i ../data/twitter/train.tsv -d ../data/twitter/dev.tsv -t ../data/twitter/test.tsv -em ../data/embed_matrix.npy
-python evaluate.py -i predictions.json -v ../data/twitter/test.tsv 
+python ../code/train_LSTM.py -i ../data/twitter/train.tsv -d ../data/twitter/dev.tsv -t ../data/twitter/test.tsv -em ../data/embed_matrix.npy
+python ../code/evaluate.py -i ../code/predictions.json -v ../data/twitter/test.tsv 
