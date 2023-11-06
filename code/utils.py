2077de54f6a94fa478bf3062b0053ae3cec782e5
Frieso Turkstra <friesoturkstra@gmail.com>
2023-11-06 12:44:51 +0100

    Merged read_corpus functions

--- code/utils.py
+++ code/utils.py
@@ -8,21 +8,6 @@ def read_corpus(corpus_file):
     with open(corpus_file, encoding='utf-8') as in_file:
         for line in in_file:
             tokens = line.strip().split('\t')
-            documents.append(tokens[0].split())
-            labels.append(tokens[1])
-
-    return documents, labels
-
-def read_corpus_lstm(corpus_file):
-    '''Reads a string and returns a list of messages in their entirety
-    and a list of corresponding labels.'''
-
-    documents = []
-    labels = []
-
-    with open(corpus_file, encoding='utf-8') as in_file:
-        for line in in_file:
-            tokens = line.strip().split('\t')
             documents.append(tokens[0])
             labels.append(tokens[1])
 
