bc788364d0b1c85d502d1eadd30b0755aaed9ddf
Frieso Turkstra <friesoturkstra@gmail.com>
2023-11-06 18:23:35 +0100

    moved scripts to separate directory

--- code/pipeline_LSTM.sh
+++
@@ -1,2 +0,0 @@
-python train_LSTM.py -i ../data/twitter/train.tsv -d ../data/twitter/dev.tsv -t ../data/twitter/test.tsv -em ../data/embed_matrix.npy
-python evaluate.py -i predictions.json -v ../data/twitter/test.tsv 
--- code/pipeline_plm.sh
+++
@@ -1,2 +0,0 @@
-python test_PLM.py -in ../data/twitter/test.tsv -twitter
-python evaluate.py -i predictions.json -v ../data/twitter/test.tsv
